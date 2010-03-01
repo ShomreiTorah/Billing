9891d962b9a5aaad9bab339892eb65abe18cef82
Schabse Laks <Schabse@GMail.com>
2010-03-01 12:08:26 -0500

    GoogleMapControl.Text
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Billing/": change = 1878]

--- Forms/PersonDetails.cs
+++ Forms/PersonDetails.cs
@@ -44,7 +44,8 @@ namespace ShomreiTorah.Billing.Forms {
 		}
 		private void personBindingSource_CurrentItemChanged(object sender, EventArgs e) { UpdateDetails(); }
 		void UpdateDetails() {
-			map.AddressString = person.Address + ", " + person.City + ", " + person.State + ", " + person.Zip;
+			map.Text = person.FullName;
+			map.AddressString = person.Address + Environment.NewLine + person.City + ", " + person.State + "  " + person.Zip;
 			details.Text = new PersonData(person).ToFullString()
 						 + "\r\n\r\nBalance Due: " + person.BalanceDue.ToString("c", CultureInfo.CurrentUICulture);
 		}
