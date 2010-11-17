83862ed8de5d81ee325199afca3bce092920d0ce
Schabse Laks <Schabse@GMail.com>
2010-11-16 19:20:52 -0500

    Rewrite HebrewCalendarEdit
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Billing/": change = 2966]

--- Controls/HebrewCalendarEdit.cs
+++ Controls/HebrewCalendarEdit.cs
@@ -1,42 +1,29 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.Drawing;
 using System.Windows.Forms;
 using DevExpress.Accessibility;
+using DevExpress.Utils;
 using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
 using DevExpress.XtraEditors.Drawing;
+using DevExpress.XtraEditors.Mask;
+using DevExpress.XtraEditors.Popup;
 using DevExpress.XtraEditors.Registrator;
 using DevExpress.XtraEditors.Repository;
 using DevExpress.XtraEditors.ViewInfo;
-using ShomreiTorah.WinForms.Controls;
-using DevExpress.XtraEditors.Mask;
-using DevExpress.Utils;
 using ShomreiTorah.Common.Calendar;
-using DevExpress.XtraEditors.Controls;
+using ShomreiTorah.WinForms.Controls;
 
 namespace ShomreiTorah.Billing.Controls {
-	sealed class HebrewCalendarEdit : PopupContainerEdit {
+	sealed class HebrewCalendarEdit : PopupBaseEdit {
 		static HebrewCalendarEdit() { RepositoryItemHebrewCalendarEdit.Register(); }
 
 		[Category("Properties")]
 		[Description("Gets an object containing properties, methods and events specific to the spin edit control.")]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
 		public new RepositoryItemHebrewCalendarEdit Properties { get { return base.Properties as RepositoryItemHebrewCalendarEdit; } }
-
-		public override string EditorTypeName {
-			get { return "HebrewCalendarEdit"; }
-		}
-
-		protected override void DoShowPopup() {
-			FlushPendingEditActions();
-			base.DoShowPopup();
-			if (EditValue == null || EditValue == DBNull.Value)
-				Properties.Calendar.SelectedDate = null;
-			else
-				Properties.Calendar.SelectedDate = (DateTime)EditValue;
-		}
+		public override string EditorTypeName { get { return "HebrewCalendarEdit"; } }
 
 		[Browsable(false)]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
@@ -44,17 +31,57 @@ namespace ShomreiTorah.Billing.Controls {
 			get { return (DateTime)EditValue; }
 			set { EditValue = value; }
 		}
+
+		protected override void DoShowPopup() {
+			FlushPendingEditActions();	//Force the DateTime to be accurate
+			base.DoShowPopup();
+		}
+
+		protected override PopupBaseForm CreatePopupForm() { return new MyPopupForm(this); }
+		class MyPopupForm : PopupBaseForm {
+			public HebrewCalendar Calendar { get; private set; }
+			public MyPopupForm(HebrewCalendarEdit owner)
+				: base(owner) {
+
+				Calendar = new HebrewCalendar { Dock = DockStyle.Fill };
+				Calendar.KeyUp += Calendar_KeyUp;
+				Calendar.DateClicked += Calendar_DateClicked;
+
+				Controls.Add(Calendar);
+			}
+			protected override Size CalcFormSizeCore() { return new Size(744, 533); }
+
+			public override void ShowPopupForm() {
+				if (OwnerEdit.EditValue == null || OwnerEdit.EditValue == DBNull.Value)
+					Calendar.SelectedDate = null;
+				else
+					Calendar.SelectedDate = (DateTime)OwnerEdit.EditValue;	//TODO: Select the bottom-right corner; selection changes
+				base.ShowPopupForm();
+				Calendar.Focus();
+			}
+
+			void Calendar_KeyUp(object sender, KeyEventArgs e) {
+				if (e.KeyCode == Keys.Enter)
+					ClosePopup();
+			}
+			void Calendar_DateClicked(object sender, HebrewDateEventArgs e) {
+				ClosePopup();
+			}
+			public override object ResultValue {
+				get { return Calendar.SelectedDate == null ? null : (object)Calendar.SelectedDate.Value.EnglishDate; }
+			}
+		}
 	}
 
 	[UserRepositoryItem("Register")]
-	sealed class RepositoryItemHebrewCalendarEdit : RepositoryItemPopupContainerEdit {
+	sealed class RepositoryItemHebrewCalendarEdit : RepositoryItemPopupBase {
 		public static void Register() {
 			EditorRegistrationInfo.Default.Editors.Add(
 				new EditorClassInfo(
 					"HebrewCalendarEdit",
 					typeof(HebrewCalendarEdit),
 					typeof(RepositoryItemHebrewCalendarEdit),
-					typeof(PopupContainerEditViewInfo),
+					typeof(PopupBaseEditViewInfo),
 					new ButtonEditPainter(),
 					true, EditImageIndexes.DateEdit,
 					typeof(PopupEditAccessible)
@@ -63,16 +90,6 @@ namespace ShomreiTorah.Billing.Controls {
 		}
 
 		public RepositoryItemHebrewCalendarEdit() {
-			Calendar = new HebrewCalendar { Dock = DockStyle.Fill };
-			Calendar.KeyUp += Calendar_KeyUp;
-			Calendar.DateClicked += Calendar_DateClicked;
-
-			PopupControl = new PopupContainerControl { Width = 744, Height = 533 };
-			PopupControl.Controls.Add(Calendar);
-
-			ShowPopupCloseButton = false;
-			PopupSizeable = false;
-
 			Mask.MaskType = MaskType.DateTime;
 
 			DisplayFormat.FormatType = EditFormat.FormatType = FormatType.DateTime;
@@ -80,23 +97,6 @@ namespace ShomreiTorah.Billing.Controls {
 
 			TextEditStyle = TextEditStyles.Standard;
 		}
-
-		void Calendar_KeyUp(object sender, KeyEventArgs e) {
-			if (e.KeyCode == Keys.Enter)
-				OwnerEdit.ClosePopup();
-		}
-		void Calendar_DateClicked(object sender, HebrewDateEventArgs e) {
-			OwnerEdit.ClosePopup();
-		}
-		protected override void RaiseQueryResultValue(QueryResultValueEventArgs e) {
-			if (Calendar.SelectedDate == null)
-				e.Value = null;
-			else
-				e.Value = Calendar.SelectedDate.Value.EnglishDate;
-		}
-
-		[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
-		public HebrewCalendar Calendar { get; private set; }
 
 		public override string EditorTypeName {
 			get { return "HebrewCalendarEdit"; }
