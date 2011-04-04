98e3b367164493316156eec1f2f353897ec69470
Schabse Laks <Schabse@GMail.com>
2011-04-04 10:50:39 -0500

    Separate AuctionItem.cs, add comments
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Billing/": change = 3458]

--- Events/Auctions/AuctionInfo.cs
+++ Events/Auctions/AuctionInfo.cs
@@ -1,15 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using ShomreiTorah.Common;
-using ShomreiTorah.Common.Calendar;
-using ShomreiTorah.Common.Calendar.Holidays;
 using System.Collections.ObjectModel;
 using System.Globalization;
+using System.Linq;
+using ShomreiTorah.Common.Calendar;
+using ShomreiTorah.Common.Calendar.Holidays;
 using ShomreiTorah.Data;
-using System.Diagnostics;
 
 namespace ShomreiTorah.Billing.Events.Auctions {
 	///<summary>Describes a single auction.</summary>
@@ -121,6 +117,7 @@ namespace ShomreiTorah.Billing.Events.Auctions {
 			new SimchasTorahShacharis(TimeSpan.FromHours(8.5), "שמחת תורה שחרית")
 		);
 
+		///<summary>Gets the complete set of auction groups.</summary>
 		public static readonly ReadOnlyCollection<AuctionGroup> Groups = new ReadOnlyCollection<AuctionGroup>(new[]{
 			new AuctionGroup("ראש השנה",		new AuctionInfo(Holiday.ראש٠השנה[1]),	new AuctionInfo(Holiday.ראש٠השנה[2])),
 			יום٠כיפור,
@@ -132,163 +129,20 @@ namespace ShomreiTorah.Billing.Events.Auctions {
 			new AuctionGroup("אחרון של פסח",	new AuctionInfo(Holiday.פסח[7]),		new AuctionInfo(Holiday.פסח[8])),
 
 			new AuctionGroup("שבועות",			/*new AuctionInfo(Holiday.שבועות[1]),*/	new AuctionInfo(Holiday.שבועות[2])),
-		});
+		});										//People are too tired for an auction after שבועות night
 		#endregion
 	}
+	///<summary>A contiguous group of auction specifications (eg, the first days of פסח).</summary>
 	class AuctionGroup {
 		public AuctionGroup(string name, params AuctionInfo[] auctions) { Name = name; Auctions = new ReadOnlyCollection<AuctionInfo>(auctions); }
+
+		///<summary>Gets the name of the entire group.</summary>
 		public string Name { get; private set; }
+		///<summary>Gets the aucitons in the group.</summary>
 		public ReadOnlyCollection<AuctionInfo> Auctions { get; private set; }
 
 		public override string ToString() {
 			return Name + " (" + Auctions.Count + " auctions)";
-		}
-	}
-	///<summary>Describes a single item in an auction, optionally including a מי שברך.</summary>
-	class AuctionItem : INotifyPropertyChanged {
-		static readonly ReadOnlyCollection<string> SortedOrder = Names.PledgeTypes.Single(p => p.Name == "Auction").Subtypes;
-
-		public AuctionItem(Person person, DateTime date, string auctionName, string itemName, bool includeמישברך) {
-			Date = date;
-			AuctionName = auctionName;
-
-			ItemName = itemName;
-			ItemIndex = SortedOrder.IndexOf(itemName);
-			Debug.Assert(ItemIndex >= 0, itemName + " is not a subtype!");
-
-			BidPledge = new AuctionPledge(this, "Auction");
-			BidPledge.PropertyChanged += (sender, e) => OnPropertyChanged("Bid" + e.PropertyName);
-			if (includeמישברך) {
-				MBPledge = new AuctionPledge(this, "מי שברך");
-				BidPledge.PropertyChanged += (sender, e) => OnPropertyChanged("MB" + e.PropertyName);
-			}
-		}
-
-		///<summary>Gets the name of the auction that the item belongs to.</summary>
-		public string AuctionName { get; private set; }
-		///<summary>Gets the pledge subtype.</summary>
-		public string ItemName { get; private set; }
-
-		///<summary>Gets the date (and time) for the item's pledges.</summary>
-		public DateTime Date { get; private set; }
-
-		///<summary>Gets the standard index of the pledge subtype; used to sort the grid.</summary>
-		public int ItemIndex { get; private set; }
-
-		public AuctionPledge BidPledge { get; private set; }
-		public AuctionPledge MBPledge { get; private set; }
-
-		///<summary>Gets the item's potential pledges.  These pledges may have null values.</summary>
-		public IEnumerable<AuctionPledge> Pledges {
-			get {
-				if (BidPledge != null) yield return BidPledge;
-				if (MBPledge != null) yield return MBPledge;
-			}
-		}
-
-		#region Expose Pledge Properties
-		public decimal? BidAmount {
-			get { return BidPledge == null ? new Decimal?() : BidPledge.Amount; }
-			set {
-				if (BidPledge != null)
-					BidPledge.Amount = value;
-			}
-		}
-		public string BidNote {
-			get { return BidPledge == null ? null : BidPledge.Note; }
-			set {
-				if (BidPledge != null)
-					BidPledge.Note = value;
-			}
-		}
-		public decimal? MBAmount {
-			get { return MBPledge == null ? new Decimal?() : MBPledge.Amount; }
-			set {
-				if (MBPledge != null)
-					MBPledge.Amount = value;
-			}
-		}
-		public string MBNote {
-			get { return MBPledge == null ? null : MBPledge.Note; }
-			set {
-				if (MBPledge != null)
-					MBPledge.Note = value;
-			}
-		}
-		#endregion
-
-		///<summary>Occurs when a property value is changed.</summary>
-		public event PropertyChangedEventHandler PropertyChanged;
-		///<summary>Raises the PropertyChanged event.</summary>
-		///<param name="name">The name of the property that changed.</param>
-		protected virtual void OnPropertyChanged(string name) { OnPropertyChanged(new PropertyChangedEventArgs(name)); }
-		///<summary>Raises the PropertyChanged event.</summary>
-		///<param name="e">An EventArgs object that provides the event data.</param>
-		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
-			if (PropertyChanged != null)
-				PropertyChanged(this, e);
-		}
-	}
-
-	class AuctionPledge : INotifyPropertyChanged {
-		public AuctionPledge(AuctionItem item, string pledgeType) {
-			Item = item;
-			PledgeType = pledgeType;
-		}
-		public AuctionItem Item { get; private set; }
-		public string PledgeType { get; private set; }
-
-		decimal? amount;
-		AliyahNote note;
-		public decimal? Amount {
-			get { return amount; }
-			set {
-				amount = value;
-				if (value == null) {
-					note = null;
-				} else {
-					if (note == null)
-						note = new AliyahNote();
-					note.Isמתנה = value == 0;	//Don't call the property setter; we don't need its logic or event
-				}
-				OnPropertyChanged("Amount");
-				OnPropertyChanged("Note");
-			}
-		}
-
-		public string Note {
-			get { return note == null ? null : note.Text; }
-			set {
-				if (value == null) {
-					note = null;
-					amount = null;
-					OnPropertyChanged("Amount");
-				} else {
-					if (note == null) note = new AliyahNote();
-
-					note.Text = value;
-					if (note.Isמתנה) {
-						amount = 0;	//Don't call the property setter; we don't need its logic or event
-						OnPropertyChanged("Amount");
-					} else if (Amount == 0 || Amount == null) {
-						amount = 18;
-						OnPropertyChanged("Amount");
-					}
-				}
-				OnPropertyChanged("Note");
-			}
-		}
-
-		///<summary>Occurs when a property value is changed.</summary>
-		public event PropertyChangedEventHandler PropertyChanged;
-		///<summary>Raises the PropertyChanged event.</summary>
-		///<param name="name">The name of the property that changed.</param>
-		protected virtual void OnPropertyChanged(string name) { OnPropertyChanged(new PropertyChangedEventArgs(name)); }
-		///<summary>Raises the PropertyChanged event.</summary>
-		///<param name="e">An EventArgs object that provides the event data.</param>
-		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
-			if (PropertyChanged != null)
-				PropertyChanged(this, e);
 		}
 	}
 }
---
+++ Events/Auctions/AuctionItem.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ShomreiTorah.Data;
+using System.Diagnostics;
+
+namespace ShomreiTorah.Billing.Events.Auctions {
+	///<summary>Describes a single item in an auction, optionally including a מי שברך.</summary>
+	class AuctionItem : INotifyPropertyChanged {
+		static readonly ReadOnlyCollection<string> SortedOrder = Names.PledgeTypes.Single(p => p.Name == "Auction").Subtypes;
+
+		internal AuctionItem(Person person, DateTime date, string auctionName, string itemName, bool includeמישברך) {
+			Person = person;
+			Date = date;
+			AuctionName = auctionName;
+
+			ItemName = itemName;
+			ItemIndex = SortedOrder.IndexOf(itemName);
+			Debug.Assert(ItemIndex >= 0, itemName + " is not a subtype!");
+
+			BidPledge = new AuctionPledge(this, "Auction");
+			BidPledge.PropertyChanged += (sender, e) => OnPropertyChanged("Bid" + e.PropertyName);
+			if (includeמישברך) {
+				MBPledge = new AuctionPledge(this, "מי שברך");
+				BidPledge.PropertyChanged += (sender, e) => OnPropertyChanged("MB" + e.PropertyName);
+			}
+		}
+
+		///<summary>Gets the name of the auction that the item belongs to.</summary>
+		public string AuctionName { get; private set; }
+		///<summary>Gets the pledge subtype.</summary>
+		public string ItemName { get; private set; }
+		///<summary>Gets the person associated with the pledges.</summary>
+		public Person Person { get; private set; }
+
+		///<summary>Gets the date (and time) for the item's pledges.</summary>
+		public DateTime Date { get; private set; }
+
+		///<summary>Gets the standard index of the pledge subtype; used to sort the grid.</summary>
+		public int ItemIndex { get; private set; }
+
+		///<summary>Gets the pledge for the auction's bid.</summary>
+		public AuctionPledge BidPledge { get; private set; }
+		///<summary>Gets the pledge for the associated מי שברך, if any.</summary>
+		public AuctionPledge MBPledge { get; private set; }
+
+		///<summary>Gets the item's potential pledges.  These pledges may have null values.</summary>
+		public IEnumerable<AuctionPledge> Pledges {
+			get {
+				if (BidPledge != null) yield return BidPledge;
+				if (MBPledge != null) yield return MBPledge;
+			}
+		}
+
+		#region Expose Pledge Properties
+		//These properties allow the pledges' properties to
+		//be databound. For non-existent pledges, they will
+		//always be null.  PropertyChanged events for these
+		//properties are forwarded in the constructor.
+		public decimal? BidAmount {
+			get { return BidPledge == null ? new Decimal?() : BidPledge.Amount; }
+			set {
+				if (BidPledge != null)
+					BidPledge.Amount = value;
+			}
+		}
+		public string BidNote {
+			get { return BidPledge == null ? null : BidPledge.Note; }
+			set {
+				if (BidPledge != null)
+					BidPledge.Note = value;
+			}
+		}
+		public decimal? MBAmount {
+			get { return MBPledge == null ? new Decimal?() : MBPledge.Amount; }
+			set {
+				if (MBPledge != null)
+					MBPledge.Amount = value;
+			}
+		}
+		public string MBNote {
+			get { return MBPledge == null ? null : MBPledge.Note; }
+			set {
+				if (MBPledge != null)
+					MBPledge.Note = value;
+			}
+		}
+		#endregion
+
+		///<summary>Occurs when a property value is changed.</summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="name">The name of the property that changed.</param>
+		protected virtual void OnPropertyChanged(string name) { OnPropertyChanged(new PropertyChangedEventArgs(name)); }
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="e">An EventArgs object that provides the event data.</param>
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, e);
+		}
+	}
+
+	///<summary>Describes an optional pledge associated with an auction.</summary>
+	class AuctionPledge : INotifyPropertyChanged {
+		public AuctionPledge(AuctionItem item, string pledgeType) {
+			Item = item;
+			PledgeType = pledgeType;
+		}
+		///<summary>Gets the item that owns this pledge.</summary>
+		public AuctionItem Item { get; private set; }
+		///<summary>Gets the pledge type (Auction or מי שברך).  The subtype is stored in the item.</summary>
+		public string PledgeType { get; private set; }
+
+		decimal? amount;
+		AliyahNote note;
+		///<summary>Gets or sets the pledge amount, or null if there is no pledge for this item.  For מתנות, this will be zero.</summary>
+		public decimal? Amount {
+			get { return amount; }
+			set {
+				amount = value;
+				if (value == null) {
+					note = null;
+				} else {
+					if (note == null)
+						note = new AliyahNote();
+					note.Isמתנה = value == 0;	//Don't call the property setter; we don't need its logic or event
+				}
+				OnPropertyChanged("Amount");
+				OnPropertyChanged("Note");
+			}
+		}
+
+		///<summary>Gets or sets the pledge note, or null if there is no pledge for this item.</summary>
+		public string Note {
+			get { return note == null ? null : note.Text; }
+			set {
+				if (value == null) {
+					note = null;
+					amount = null;
+					OnPropertyChanged("Amount");
+				} else {
+					if (note == null) note = new AliyahNote();
+
+					note.Text = value;
+					if (note.Isמתנה) {
+						amount = 0;	//Don't call the property setter; we don't need its logic or event
+						OnPropertyChanged("Amount");
+					} else if (Amount == 0 || Amount == null) {
+						amount = 18;
+						OnPropertyChanged("Amount");
+					}
+				}
+				OnPropertyChanged("Note");
+			}
+		}
+
+		///<summary>Occurs when a property value is changed.</summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="name">The name of the property that changed.</param>
+		protected virtual void OnPropertyChanged(string name) { OnPropertyChanged(new PropertyChangedEventArgs(name)); }
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="e">An EventArgs object that provides the event data.</param>
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, e);
+		}
+	}
+}
