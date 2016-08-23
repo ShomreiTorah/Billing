dec6840704ead76e81014b68eeb9aedeec671c5b
SLaks <Dev@SLaks.net>
2016-08-23 19:31:10 -0400

    PaymentImport: Add import form
    Currently with designer error

---
+++ ShomreiTorah.Billing.PaymentImport/ImportForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Composition;
+using DevExpress.XtraGrid.Views.Base;
+using ShomreiTorah.WinForms.Controls;
+using ShomreiTorah.Data;
+using ShomreiTorah.WinForms.Forms;
+
+namespace ShomreiTorah.Billing.PaymentImport {
+	[Export]
+	public partial class ImportForm : XtraForm {
+		readonly ViewModel viewModel;
+
+		[ImportingConstructor]
+		public ImportForm(ViewModel viewModel) {
+			InitializeComponent();
+			startDate.EditValue = DateTime.Today.AddDays(-14);
+			this.viewModel = viewModel;
+			viewModelBindingSource.DataSource = viewModel;
+			viewModel.PropertyChanged += ViewModel_PropertyChanged;
+		}
+
+		private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+			switch (e.PropertyName) {
+				case nameof(viewModel.Person):
+					peopleView.FocusedRowHandle = peopleView.GetRowHandle(
+						viewModel.MatchingPeople.IndexOf(viewModel.Person));
+					break;
+			}
+			import.Enabled = viewModel.CurrentPayment != null && viewModel.Person != null;
+		}
+
+		private void availablePaymentsView_FocusedRowObjectChanged(object sender, FocusedRowObjectChangedEventArgs e) {
+			viewModel.CurrentPayment = (PaymentInfo)e.Row;
+		}
+
+		private void peopleView_FocusedRowObjectChanged(object sender, FocusedRowObjectChangedEventArgs e) {
+			viewModel.Person = (Person)e.Row;
+		}
+
+		private void refresh_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+			LoadPayments();
+		}
+
+		private void import_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+			viewModel.Import();
+		}
+		protected override void OnShown(EventArgs e) {
+			base.OnShown(e);
+			LoadPayments();
+		}
+		void LoadPayments() {
+				var date = (DateTime)startDate.EditValue;
+			ProgressWorker.Execute(MdiParent, p => {
+				p.Caption = "Loading payments after " + date.ToShortDateString();
+				viewModel.LoadPayments(date);
+			}, cancellable: false);
+		}
+	}
+}
--- ShomreiTorah.Billing.PaymentImport/TriggerButton.cs
+++ ShomreiTorah.Billing.PaymentImport/TriggerButton.cs
@@ -13,13 +13,20 @@ namespace ShomreiTorah.Billing.PaymentImport {
 		[Import]
 		public Forms.MainForm MainForm { get; set; }
 
+		[Import]
+		public ExportFactory<ImportForm> ImportFactory { get; set; }
+
 		public override string Page => "Data";
 		public override string Group => "Payments";
 		public override bool BeginGroup => true;
 
 		public override BarItem CreateItem() {
 			var item = new BarButtonItem { Caption = "Import Credit Card Payments" };
-			item.ItemClick += delegate { Dialog.Show("TODO", System.Windows.Forms.MessageBoxIcon.Information); };
+			item.ItemClick += delegate {
+				var form = ImportFactory.CreateExport().Value;
+				form.MdiParent = MainForm;
+				form.Show();
+			};
 			return item;
 		}
 	}
