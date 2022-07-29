c739d465d1ed98ce23c9c7cf80e02e630d27652c
SLaks <Dev@SLaks.net>
2022-07-29 10:34:19 -0400

    v10.3.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using ShomreiTorah.Common.Updates;
 
-[assembly: Updatable("ShomreiTorah.Billing", "10.3.3")]
+[assembly: Updatable("ShomreiTorah.Billing", "10.3.4")]
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
